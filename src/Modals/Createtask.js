34fb504baad828a5d9290c4b52d7bdd7fb315fff
Shubham <shubhamn@soniinfo.com>
2022-09-16 12:29:26 +0530

    Task description creation

--- src/Modals/Createtask.js
+++ src/Modals/Createtask.js
@@ -1,26 +1,53 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
-function Createtask({modal,toggle}) {
-  return (
-    <Modal isOpen={modal} toggle={toggle} >
-      <ModalHeader toggle={toggle}>Modal title</ModalHeader>
-      <ModalBody>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-         
-        </ModalBody>
-        <ModalFooter>
-          <Button color="primary" onClick={toggle}>
-            Do Something
-          </Button>{' '}
-          <Button color="secondary" onClick={toggle}>
-            Cancel
-          </Button>
-        </ModalFooter>
-  </Modal>
-  )
+function Createtask({ modal, toggle }) {
+
+     const [taskname, setTaskname] = useState('')
+     const [description, setDescription] = useState('')
+
+    return (
+        <Modal isOpen={modal} toggle={toggle} >
+            <ModalHeader toggle={toggle}>Create Task </ModalHeader>
+            <ModalBody>
+                <form>
+                    <div>
+                        <label> Task Name</label>
+                        <input type='text' className='inputstyle' value={taskname}/>
+                    </div>
+                    <div>
+                    <label>Description</label>
+                     <textarea rows ='5' className='textarea' value={description}> </textarea>
+                    </div>
+                    <div>
+                    <label> Date of creation</label>
+                     <textarea rows ='5' className='textarea' value={''}> </textarea>
+                    </div>
+                    <div>
+                    <label> Date of Updation</label>
+                     <textarea rows ='5' className='textarea' value={''}> </textarea>
+                    </div>
+                    <div>
+                    <label> End date of the task </label>
+                     <textarea rows ='5' className='textarea' value={''}> </textarea>
+                    </div>
+                    <div>
+                    <label> Status of the task </label>
+                     <textarea rows ='5' className='textarea' value={''}> </textarea>
+                    </div>
+                </form>
+            </ModalBody>
+            <ModalFooter>
+                <Button color="primary" onClick={toggle}>
+                    Create
+                </Button>{' '}
+                <Button color="secondary" onClick={toggle}>
+                    Cancel
+                </Button>
+            </ModalFooter>
+        </Modal>
+    )
 }
 
 export default Createtask
