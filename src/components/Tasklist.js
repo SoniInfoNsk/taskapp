eca5149b132e88411cd19386c8df1c6fb0b49cf0
Shubham <shubhamn@soniinfo.com>
2022-09-16 10:49:59 +0530

    Popup Creation

---
+++ src/components/Tasklist.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+
+
+function Tasklist() {
+    return (
+        <div>
+            <div className='header text-center'>
+                <h3>Task List</h3>
+                <Button variant="primary"   className='buttoncontainer'>Create Task </Button>{' '}
+            </div>
+             <div>
+
+
+
+
+             </div>
+ 
+        </div>
+
+
+
+
+    )
+}
+
+export default Tasklist
